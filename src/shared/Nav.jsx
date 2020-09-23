4ad2bea52adc6f962f5d10f75e074fe6e9bafb05
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-09-23 17:48:57 -0400

    nav h1 styling

--- src/shared/Nav.jsx
+++ src/shared/Nav.jsx
@@ -1,22 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Route, withRouter } from 'react-router-dom'
 
 function Nav() {
+
+  let [headerShift, setHeaderShift] = useState({
+    projects: false,
+    resume: false,
+    about: false
+  })
+
+  let clickProjects = () => {
+    setHeaderShift({
+      projects: true,
+      resume: false,
+      about: false
+    })
+    console.log("headerShift", headerShift)
+  }
+
+  let clickResume = () => {
+    setHeaderShift({
+      projects: false,
+      resume: true,
+      about: false
+    })
+    console.log("headerShift", headerShift)
+  }
+
+  let clickAbout = () => {
+    setHeaderShift({
+      projects: false,
+      resume: false,
+      about: true
+    })
+    console.log("headerShift", headerShift)
+  }
+
+  let rendProjects = () => {
+    if (headerShift.projects === true) {
+      return <h1 className="sect-title" >Projects</h1>
+    } else {
+      return <h3>Projects</h3>
+    }
+  }
+
+  let rendResume = () => {
+    if (headerShift.resume === true) {
+      return <h1 className="sect-title" >Resume</h1>
+    } else {
+      return <h3>Resume</h3>
+    }
+  }
+
+  let rendAbout = () => {
+    if (headerShift.about === true) {
+      return <h1 className="sect-title" >About Me</h1>
+    } else {
+      return <h3>About Me</h3>
+    }
+  }
+
   return (
     <div>
 
       <div className="menu-box col-12">
 
-        <Link to="/main/projects">
-          <h3>Projects</h3>
+        <Link to="/main/projects" onClick={() => clickProjects()}>
+          {rendProjects()}
         </Link>
 
-        <Link to="/main/resume">
-          <h3>Resume</h3>
+        <Link to="/main/resume" onClick={() => clickResume()}>
+          {rendResume()}
         </Link>
 
-        <Link to="/main/aboutme">
-          <h3>About Me</h3>
+        <Link to="/main/aboutme" onClick={() => clickAbout()}>
+          {rendAbout()}
         </Link>
       </div>
     </div>
