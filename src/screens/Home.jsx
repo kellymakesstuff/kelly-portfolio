7eadc31f1c5d749537496413805313f0054e6c3c
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-09-24 10:52:54 -0400

    phone changes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,14 +9,14 @@ function Home() {
 
     <div className="home-msg col-10">
       <h1>Hi, I'm Kelly!</h1>
-      <br /><br /><br /><br />
+      <br />
       <h3 className="home-msg-text">I am a true New Yorker who thrives in dynamic spaces.
       People, the teams they compose, and the things they create
       are my life-blood. I am a total software automation nerd,
       and love creating streamlined workflows, integrating
       systems as naturally as possible. I've been told that
    only 20% of my Excel jokes are un-punny.</h3>
-      <br /><br /><br /><br />
+      <br />
 
 
       <Link to="/main">
