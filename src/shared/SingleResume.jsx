08d79e5a6076a385c66ceb6a221b8c62597dd7a3
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-09-24 18:19:57 -0400

    job title color

--- src/shared/SingleResume.jsx
+++ src/shared/SingleResume.jsx
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom'
 function singleResume({ resumeItems }) {
   return <>
     <div className="single-resume-item">
-      <h4>{resumeItems.company}, <span className="font-italic">{resumeItems.title} [{resumeItems.start} - {resumeItems.end}]</span></h4>
+      <h4><span className="sect-title">{resumeItems.company}, <span className="font-italic">{resumeItems.title} [{resumeItems.start} - {resumeItems.end}]</span></span></h4>
       <ul>{resumeItems.bullets.map(bullet => <li>{bullet}</li>)}</ul>
     </div>
   </>
