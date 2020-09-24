ce1ef16befe071a0c148d2f87e7055d6091dc241
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-09-23 20:09:15 -0400

    title styling

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -8,6 +8,10 @@ function Header() {
   return <>
 
     <div className="row col-12">
+      <div className="name-overlay">
+        <p className="title-text">kelly bergin, mfa</p>
+        <p className="under-text">software engineer</p>
+      </div>
       <div className="circle-one"></div>
       <div className="circle-two"></div>
       <div className="circle-three"></div>
